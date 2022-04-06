76cfa1eadbd1ac597c5612300fb7f876dc76b8fd
cealmira <85953617+cealmira@users.noreply.github.com>
2022-04-06 18:45:19 -0500

    Branch dev/dash

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 from db.conn import mysql_conn
 
 ms = mysql_conn('localhost', 'root', 'root', 3307, 'ENOE')
-
 tables = ['COE1', 'COE2', 'SDEM', 'VIV', 'HOG']
 
+# Carga del esqueleto de la db
 for i in range(11,22):
     for j in range(1,5):
         for k in (tables):
             #print(f'{j * 100 + i}, {k}')
             ms.schema_db(k, (j * 100 + i))
 
-
-#ms.select_trim_df(3)
 
